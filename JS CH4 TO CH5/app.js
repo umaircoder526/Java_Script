9e9b2adc800150f4fd4a7d9fd419b856ef05d6c1
umaircoder526 <umairarfeen793@gmail.com>
2025-10-26 12:34:11 +0500

    JS ASSIGNMENT CH4 TO CH5

---
+++ JS CH4 TO CH5/app.js
@@ -0,0 +1,13 @@
+// Legal variable 
+// var $name = "Hasnain" , na1meVariable = "khan" , na_me = "jadoon" ;
+
+// illegal variable 
+// /* var 1stName = 28; , var #hkah = "jdkw" */ -khna = "pakistan"
+
+
+// document.writeln("<h1>RULES FOR NAMING JS VARIABLE</h1>");
+// document.writeln("<h4>Variable Only Contain $name , na_me , nam1e</h4>");
+// document.writeln("<h4>Variable Must Begin With $name , na_me , nam1e</h4>");
+// document.writeln("<h4>Variable is Case Sensitive</h4>");
+// document.writeln("<h4>Variable Shuld Not be JS Keyword</h4>");
+
