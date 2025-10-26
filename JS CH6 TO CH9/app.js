8ba0b03fce76d512852e28bc50b5774ae868be65
umaircoder526 <umairarfeen793@gmail.com>
2025-10-26 12:37:16 +0500

    JS ASSIGNMENT CH6 TO CH9

---
+++ JS CH6 TO CH9/app.js
@@ -0,0 +1,205 @@
+// console.log("js running==>");
+
+// var a = 45 , b = 55 ;
+// var result = a + b;
+// document.writeln( "SUM OF 45 AND 55 IS ==>" ,result);
+
+// var c =3 , d =5;
+// var total = c * d;
+// document.writeln("<p>MULTIPLY 3 BY 5 IS EQUALS TO ===>" , total);
+
+// var c =3 , d =5;
+// var total = c - d;
+// document.writeln("<p>SUBTRACT 3 BY -5 IS EQUALS TO ===>" , total);
+
+
+// var c =3 , d =5;
+// var total = c / d;
+// document.writeln("<p> 3 DIVIDED BY 5 IS EQUALS TO ===>" , total);
+
+
+// var a = 5;
+// document.writeln("<p>INITIAL VALUE BEFORE INCREMENT ==>" , a);
+// var a = 5;
+// ++a;
+// var b =7;
+// var res = a + b;
+// document.writeln("<p>INITIAL VALUE AFTER INCREMENT ==>" , a);
+// document.writeln( "<p>SUM OF INCREMENT VALUE AND + 7 ==>" , a + b);
+// var c = 13;
+// document.writeln("<p>VALUE BEFORE DECREMEMNT==>" , c);
+// --c;
+// document.writeln("<p>VALUE AFTER DECREMEMNT==>" , c);
+// var d = 3;
+// var result = c % d ;
+// document.writeln("<p>VALUE OF DECREMENT  % 3 IS ==>" ,result);
+
+
+// var ticket = 600;
+// document.writeln("<P>COST OF ONE TICKET IS ==>" , ticket);
+// var total = 5 * 600;
+// document.writeln("<P>COST OF Total TICKET IS ==>" , total);
+
+
+// let tableNum = prompt("Enter Table Number", 5 )
+// (tableNum , "X" , 1 ,  "=" , tableNum * 1)
+// (tableNum , "X" , 2 ,  "=" , tableNum * 2)
+// (tableNum , "X" , 3 ,  "=" , tableNum * 3)
+// (tableNum , "X" , 4 ,  "=" , tableNum * 4)
+// (tableNum , "X" , 5 ,  "=" , tableNum * 5)
+// (tableNum , "X" , 6 ,  "=" , tableNum * 6)
+// (tableNum , "X" , 7 ,  "=" , tableNum * 7)
+// (tableNum , "X" , 8 ,  "=" , tableNum * 8)
+// (tableNum , "X" , 9 ,  "=" , tableNum * 9)
+// (tableNum ,  "X",  10 , "=" , tableNum * 10)
+
+
+// document.writeln("<p>4 X 1 = 4")
+// document.writeln("<p>4 X 2 = 8")
+// document.writeln("<p>4 X 3 = 12")
+// document.writeln("<p>4 X 4 = 16")
+// document.writeln("<p>4 X 5 = 20")
+// document.writeln("<p>4 X 6 = 24")
+// document.writeln("<p>4 X 7 = 28")
+// document.writeln("<p>4 X 8 = 32")
+// document.writeln("<p>4 X 9 = 36")
+// document.writeln("<p>4 X 10 = 40")
+
+
+// var celcius = 25;
+// var fahrenheit = 70;
+// var celcius = ( 70 - 32) * 5/9;
+// document.writeln( "<p>converting 70 fahrenheit into celcius==>",celcius);
+// var fahrenheit = (25 * 9/5) +32;
+// document.writeln( "<p>converting 25  celcius into fahrenheit ==>",fahrenheit);
+
+
+
+// var firstitem = 300;
+// var seconditem = 200;
+// var order1 = 4;
+// var order2 = 8;
+// var shippingchar = 100;
+// var Totalcost1 = 300 * 4 ;
+// var Totalcost2 = 200 * 8;
+// var result = Totalcost1 + Totalcost2 + shippingchar;
+// document.writeln("<p>Total Price of Your Equipments is Equal To " , result);
+
+
+
+// var totalmarks = 100;
+// document.writeln("<P>TOTAL MARKS IS " , totalmarks);
+// var obtainedmarks = 83.6;
+// document.writeln("<p>OBTAINED MARKS IS " , obtainedmarks);
+// var percentage = 83.6 / 100 * 100;
+// document.writeln("<p>PERCENTAGE IS ==>" , percentage);
+
+
+
+
+
+
+
+// var dollars = 25;
+// var riyal = 10;
+// var result = 104.8 * 20 + 28 * 10;
+// document.writeln("<p>TOTAL COST OF TWO CURRENCY IS EQUAL TO ==>" , result);
+
+
+
+
+
+// var a =5 , b = 6 , c =98 , d =65;
+// var bodmasresult = a* b / c + (c*a) + b * c;
+// console.log("<P>see 4 * 4 + 67 / 5 (4*7) is equal to ==>" , bodmasresult);
+
+
+
+
+
+
+
+// var cy = 2025;
+// var by = 2009;
+// document.writeln("current year---> " , cy);
+// document.writeln("<p>birth  year---> " , by);
+// var ta = 2025 - 2009;
+// document.writeln("<p>my age ---> " , ta);
+
+
+
+// var cr = 20;
+// var rcr = 2 * 3.142 * 20 ;
+// console.log(rcr);
+// var aoc = 3.142 * 20 * 20 ;
+// console.log(aoc);
+
+
+
+
+
+
+
+
+// var n = "choco";
+// var age = 16;
+// var mage = 88;
+// var one = 12;
+// var total = 88 -16 * 12;
+// console.log(total);
+
+
+
+
+// ch 6 start here
+
+
+// var a = 10;
+// document.writeln("RESULT " , a);
+// a++;
+// document.writeln("<p>After increment  the value is ===>" , a)
+// --a;
+// document.writeln("<p>After decrement the value is ===>" , a);
+
+
+
+// var a =12 , b = 10;
+// var result = --a - --b + ++b + b--;
+// console.log(result);
+
+
+
+// --a; pre decrememnt
+// a--; post decrement  
+// ++a; pre decrement 
+// // a++; post decrememnt 
+
+
+// let greet = "UserName";
+// let a = prompt("Enter Your Name" , greet);
+
+// let tableNum = prompt("Enter Number"  , 5);
+// document.writeln("<p>" , tableNum , "x" , 1 , "="  , tableNum * 1 , "</p>"); 
+// document.writeln("<p>" , tableNum , "x" , 2 , "="  , tableNum * 2 , "</p>")
+// document.writeln("<p>" , tableNum , "x" , 3 , "="  , tableNum * 3 , "</p>");
+// document.writeln("<p>" , tableNum , "x" , 4 , "="  , tableNum * 4 , "</p>");
+// document.writeln("<p>" , tableNum , "x" , 5 , "="  , tableNum * 5 , "</p>");
+// document.writeln("<p>" , tableNum , "x" , 6 , "="  , tableNum * 6 , "</p>");
+// document.writeln("<p>" , tableNum , "x" , 7 , "="  , tableNum * 7 , "</p>");
+// document.writeln("<p>" , tableNum , "x" , 8 , "="  , tableNum * 8 , "</p>");
+// document.writeln("<p>" , tableNum , "x" , 9 , "="  , tableNum * 9 , "</p>");
+// document.writeln("<p>" , tableNum , "x" , 10 , "="  , tableNum * 10 , "</p>");
+
+
+// var obt1 = prompt("Enter Mathematics Number" , 100);
+// var obt2 = prompt("Enter English Number" , 100);
+// var obt3 = prompt("Enter Urdu Number" , 100);
+// let totalobt =  obt1 / 100 * 100 ;
+// let totalobt2 = obt2 / 100 * 100;
+// let totalobt3 = obt3 / 100 *100;
+// document.writeln("<h3>Your Mathematics Percentage </h3>" ,totalobt , "%")
+// document.writeln(" <h3>Your English Percentage</h3> " ,totalobt2 , "%")
+// document.writeln("<h3>Your Urdu Percentage</h3> "  ,totalobt3 , "%")
+
+
+// ch6 to ch9 is end here 
