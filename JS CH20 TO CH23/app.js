d5b7a77c52fdb86ca59f9869f848e4b581576c10
umaircoder526 <umairarfeen793@gmail.com>
2025-11-09 03:22:21 +0500

    Assignment complete

---
+++ JS CH20 TO CH23/app.js
@@ -0,0 +1,45 @@
+console.log("Hello World!");
+
+
+// QNO1 STARTS HERE 
+// let firstName = prompt("What Is Your First Name?");
+// let lastName = prompt("What Is Your Last Name?");
+// let fullNamee = firstName + " " + lastName;
+// console.log(fullName);
+
+// QNO1 ENDS HERE 
+
+// QNO2 STARTS HERE 
+// let userFavMobile = prompt("Enter Your Favourite Mobile Name!");
+// console.log(userFavMobile.length)
+
+// QNO2 STARTS HERE 
+
+// QNO3 STARTS HERE
+// let user = "Zainab";
+// let letter = "n";
+// let indexOfLetter = user.indexOf(letter);
+// console.log("Index of " + letter + " is: " + indexOfLetter);
+// QNO3 ENDS HERE 
+
+// QNO4 STARTS HERE 
+// let user = "Hello World!";
+// let letter = "l";
+// let result = user.lastIndexOf(letter);
+// console.log(result);
+// QNO4 ENDS HERE 
+
+// QNO5 STARTS HERE 
+
+// let a = "Pakistan";
+// let index = a[3];
+// console.log("At Index 3 Is ====> " ,index);
+
+// QNO5 ENDS HERE 
+
+// QNO6 STARTS HERE 
+// let firstName = prompt("What is Your First Name!");
+// let lastName = prompt("What is Your Last Name!");
+// let fullName = firstName.concat(" " , lastName);
+// console.log(fullName);
+// QNO6 ENDS HERE
